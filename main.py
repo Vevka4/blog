a3e8e2918a0d865e78ed34381799fdb01d2e6cf1
Vevak4 <148484532+Vevka4@users.noreply.github.com>
2024-09-05 20:59:57 +0300

    add web pages and db queries

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+import db
+
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template("base.html")
 
 
+@app.route("/categories")
+def category_list():
+    return render_template("category_list.html",
+                            category_list=db.getCategories())
+                                
 
 
+@app.route("/posts")
+def post_list():
+    return render_template("post_list.html",
+                            post_list=db.getPosts())
 
-
+@app.route("/categories/<id>")
+def posts_by_category(id):
+    return render_template("post_list.html",
+                            post_list=db.getPostsByCategory(id))
 
 
 if __name__ == "__main__":
